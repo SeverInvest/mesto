add272a0029ab7c2449ccf6c9f65a323ff964787
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2022-09-26 23:36:09 +0300

    finish uffff...

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,13 @@ let popupAboutOld = document.querySelector('.traveler__about');
 let popupNameNew = overlay.querySelector('.popup__name');
 let popupAboutNew = overlay.querySelector('.popup__about');
 const popupSubmit = overlay.querySelector('.popup__submit');
+const popupClose = overlay.querySelector('.popup__close');
 
 function overlayInvisible(event) {
   if (event.target.className == "traveler__button-correct") {
     popupNameNew.value = popupNameOld.textContent;
     popupAboutNew.value = popupAboutOld.textContent;
-  } else {
+  } else if(event.target.className == "popup__submit"){
     event.preventDefault();
     popupNameOld.textContent = popupNameNew.value;
     popupAboutOld.textContent = popupAboutNew.value;
@@ -20,6 +21,7 @@ function overlayInvisible(event) {
 
 popupOpen.addEventListener('click', overlayInvisible);
 popupSubmit.addEventListener('click', overlayInvisible);
+popupClose.addEventListener('click', overlayInvisible);
 
 
 
