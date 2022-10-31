9bcc4033b9983b8cad0125f7afbfa1b3443af843
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2022-10-31 07:11:06 +0300

    fix _getTemplate and _removeCard

--- scripts/Card.js
+++ scripts/Card.js
@@ -6,32 +6,35 @@ export default class Card {
   constructor(name, link, templateCardSelector) {
     this._name = name;
     this._link = link;
-    this._cardTemplate = this._getTemplate(templateCardSelector);
+    this._templateCardSelector = templateCardSelector
+    this._cardElement = this._getTemplate();
   };
 
-  _getTemplate(templateCardSelector) {
-    return document.querySelector(templateCardSelector).content;
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._templateCardSelector)
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
+
+    return cardElement;
   };
 
-  _toggleLike(evt) {
-    evt.target.classList.toggle('card__heart_active');
+  _toggleLike() {
+    this._cardElement.querySelector('.card__heart').classList.toggle('card__heart_active');
   };
 
-  _removeCard(evt) {
-    // этот код не работает
-    // TypeError: undefined is not an object (evaluating 'this._cardElement.remove')
-    //this._cardElement.remove(); 
-    evt.target.closest('.card').remove();
+  _removeCard() {
+    this._cardElement.remove(); 
   };
 
   _setEventListeners() {
     this._cardElement.querySelector('.card__photo').addEventListener('click', renderPopupBigPhoto);
-    this._cardElement.querySelector('.card__heart').addEventListener('click', this._toggleLike);
-    this._cardElement.querySelector('.card__remove-button').addEventListener('click', this._removeCard);
+    this._cardElement.querySelector('.card__heart').addEventListener('click', () => this._toggleLike());
+    this._cardElement.querySelector('.card__remove-button').addEventListener('click', () => this._removeCard());
   }
 
   createCard() {
-    this._cardElement = this._cardTemplate.cloneNode(true);
     this._cardElement.querySelector('.card__description').textContent = this._name;
     this._cardElement.querySelector('.card__photo').src = this._link;
     this._cardElement.querySelector('.card__photo').alt = this._name;
